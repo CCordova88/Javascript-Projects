b8e9d4fe7383190442147277803df9d170f7b70b
CCordova88 <62787560+CCordova88@users.noreply.github.com>
2020-05-10 16:12:19 -0600

    Add second javascript assignment

---
+++ Basic_Javascript_Projects/JS/basic_javascript_2.js
@@ -0,0 +1,9 @@
+function dateTime() {document.getElementById("displayhere").innerHTML = Date();
+}
+
+function conFunction() {
+    var sentence = "Drax is my seven-month old cat.";
+    sentence += " He lives up to his namesake by trying";
+    sentence += " to fight other animals that are much larger that him.";
+    document.getElementById("string").innerHTML = sentence;
+}
