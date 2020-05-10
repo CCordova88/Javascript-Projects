71bbff887e11514e1dbf0b46c863f3059b7a8e23
CCordova88 <62787560+CCordova88@users.noreply.github.com>
2020-05-10 13:24:57 -0600

    Add Basic Javascript Projects folder

---
+++ Basic Javascript Projects/JS/basic_javascript_1.js
@@ -0,0 +1,13 @@
+window.alert("Hello,Cameron!");
+
+var A = "Have a great day, Cameron.<br>",
+    B = "If my cat could speak, he would say; \"get off your fat ass and play with me\"<br>."
+    +"But, instead he just meows really loud while giving me an awkward stare.<br>",
+    A = A.fontcolor("red"),
+    B = B.fontcolor("blue"); 
+
+document.write(A);
+
+document.write(B);
+
+document.write(111*6)
