66544dc71d6518b943d6103e376a54d9d05a8631
CCordova88 <62787560+CCordova88@users.noreply.github.com>
2020-05-13 19:42:51 -0600

    Add fifth javascript assignment

---
+++ Basic_Javascript_Projects/JS/basic_javascript_5.js
@@ -0,0 +1,36 @@
+document.write(typeof 13);
+
+document.write(6E520);
+
+document.write(-7E650);
+
+document.write(76 > 85);
+
+console.log(6 * 111);
+
+document.write("13" + 6);
+
+console.log(6 < 5);
+
+document.write((6*111) == (1539-873));
+
+document.write(35 == (5*6))
+
+A = "thirteen forty nine";
+B = "one thousand three hundred and forty nine";
+C = 1349;
+D = 1349;
+E = "1349"
+document.write(C === D);
+document.write(A === C);
+document.write(D === E);
+document.write(A === B);
+
+document.write(7 > 5 && 3 < 6);
+document.write(6 > 4 && 8 < 1);
+
+document.write(9 > 2 || 5 < 5);
+document.write(8 == 7 || 4 > 8);
+
+document.write(!(60 > 25))
+document.write(!(95 < 52))
